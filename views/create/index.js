9488e13f5759ad0d965989831c469f7a93207c08
Gabriel <gabrielenriquecas@gmail.com>
2023-11-28 19:13:14 -0400

    improve

--- views/create/index.js
+++ views/create/index.js
@@ -13,15 +13,28 @@ const NAME_VALIDATION = /^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]{1,15}\s){1}[A-ZÁ�
 const EMAIL_VALIDATION = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
 // validations
-const nameValidation = false;
+let nameValidation = false;
 const emailValidation = false;
 
-const validation = (input, regex) => {
-
+const validation = (input, regexValidation) => {
+    if (input.value === '') {
+        input.classList.remove('outline-red-700', 'focus:outline-cyan-700');
+        input.classList.add('outline-2', 'outline', 'focus:outline-cyan-700');
+    } else if (regexValidation) {
+        input.classList.remove('outline-red-700', 'focus:outline-cyan-700');
+        input.classList.add('outline-green-700', 'outline-2', 'outline');
+    } else {
+        input.classList.remove('outline-green-700', 'focus:outline-cyan-700');
+        input.classList.add('outline-red-700', 'outline-2', 'outline');
+    }
 };
+
+nameInput.addEventListener('input', e => {
+    nameValidation = NAME_VALIDATION.test(e.target.value);
+    validation(nameInput, nameValidation);
+});
 
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    btn.disabled = nameInput;
 });
