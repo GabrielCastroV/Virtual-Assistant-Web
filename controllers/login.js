198760eb4eeb5dd0d728a522b1924c4695251993
Gabriel <gabrielenriquecas@gmail.com>
2023-12-02 23:36:25 -0400

    text changed

--- controllers/login.js
+++ controllers/login.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 loginRouter.post('/', async (req, res) => {
     const { email, password } = req.body;
     if (!(email == process.env.USER_ADMIN && password == process.env.PASSWORD_ADMIN)) {
-        return res.status(401).json({ error: 'not logged tho' });
+        return res.status(401).json({ error: 'Usuario o clave inválida' });
     }
     console.log('logged');
     return res.sendStatus(202);
