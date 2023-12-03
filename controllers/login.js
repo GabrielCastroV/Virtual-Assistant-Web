218f36023478cee8004e7a3329da4f0522760183
Gabriel <gabrielenriquecas@gmail.com>
2023-12-02 23:25:19 -0400

    backend fixed

--- controllers/login.js
+++ controllers/login.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 
 loginRouter.post('/', async (req, res) => {
     const { email, password } = req.body;
-    if (email == process.env.USER_ADMIN && password == process.env.PASSWORD_ADMIN) {
-        console.log('logged');
-        return res.status(201);
-    } else {
-        return res.status(401).json({ error: 'not logged' });
+    if (!(email == process.env.USER_ADMIN && password == process.env.PASSWORD_ADMIN)) {
+        return res.status(401).json({ error: 'not logged tho' });
     }
+    console.log('logged');
+    return res.sendStatus(202);
 });
 
 module.exports = loginRouter;
