6a131a31e6351ccfd4c1f986d9032a2b1a070780
Gabriel <gabrielenriquecas@gmail.com>
2023-12-09 16:29:47 -0400

    inscripciones improved

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,17 +1,25 @@
 const PagoMovil = require('../models/pagomovil');
 const Registration = require('../models/registrations');
 const User = require('../models/user');
+const Course = require('../models/courses');
 const { getDollarPrices } = require('venecodollar');
 
 const dashboardRouter = require('express').Router();
 
 dashboardRouter.get('/', async (request, res) => {
-    const students = await User.find();
-    const info = await getDollarPrices();
-    const dollarPrice = info[5].dollar;
-    const registrations = await Registration.find();
-    const pagoMovil = await PagoMovil.find();
-    return res.status(200).json({ students, dollarPrice, registrations, pagoMovil });
+    try {
+        const students = await User.find();
+        const info = await getDollarPrices();
+        const dollarPrice = info[5].dollar;
+        const registrations = await Registration.find();
+        const pagoMovil = await PagoMovil.find();
+        const courses = await Course.find();
+        return res.status(200).json({ students, dollarPrice, registrations, pagoMovil, courses });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Hubo un error en el servidor :(' });
+    }
+
 });
 
 module.exports = dashboardRouter;
--- views/dashboard/index.js
+++ views/dashboard/index.js
@@ -5,6 +5,8 @@ const bcv = document.querySelector('#bcv');
 const textLoader = document.querySelector('#text-loader');
 const mainContainer = document.querySelector('#main-container');
 const loaderScreen = document.querySelector('#loader-screen');
+const registrationContainer = document.querySelector('#registrations-scroll');
+const moduleContainer = document.querySelector('#modules-scroll');
 
 (async () => {
     try {
@@ -26,6 +28,66 @@ const loaderScreen = document.querySelector('#loader-screen');
         pendingRegistration.innerHTML = unverifiedRegistration.length;
         monthlyPayment.innerHTML = unverifiedPagoMovil.length;
 
+        // relleno los contenedores con sus pagos pendientes
+        for (let i = 0; i < unverifiedRegistration.length; i++) {
+            if (unverifiedRegistration[i].currency === 'USD') {
+                // en caso de ser el pago en tarjeta de credito
+                registrationContainer.innerHTML += `
+                <div id="registrations-info-container" class="w-full  bg-allports-100 shadow-md p-4 rounded-lg flex flex-col justify-center items-center">
+                    <div>Tarjeta de crédito 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                        </svg>
+                  </div>
+                    <p class="break-all text-sm text-allports-900 lg:text-base"> 
+                        <span class="font-semibold text-allports-950">Nombre:</span> ${unverifiedRegistration[i].name} <br>
+                        <span class="font-semibold text-allports-950">Email:</span>  ${unverifiedRegistration[i].email} <br>
+                        <span class="font-semibold text-allports-950">Teléfono:</span>  ${unverifiedRegistration[i].phone} <br>
+                        <span class="font-semibold text-allports-950">Curso:</span>  ${unverifiedRegistration[i].course} (${'pendiente'})<br>
+                        <span class="font-semibold text-allports-950">Monto:</span>   ${unverifiedRegistration[i].total} ${unverifiedRegistration[i].currency}<br>
+                        <span class="font-semibold text-allports-950">Orden:</span> #${unverifiedRegistration[i].order_id}
+                    </p>
+                    <div id="btn-container" class="flex gap-4">
+                        <button id="registrations-btn" class="bg-allports-700 hover:bg-allports-800 text-sm p-4 text-allports-50 rounded-lg mt-4 font-semibold">Confirmar</button>
+                        <button id="registrations-btn" class="bg-red-400 hover:bg-red-500 text-sm p-4 text-allports-50 rounded-lg mt-4 font-semibold">Rechazar</button>
+                    </div>
+                </div>
+                `;
+            } else if (unverifiedRegistration[i].currency === 'VES') {
+                // en caso de ser un pago movil
+                registrationContainer.innerHTML += `
+                <div id="registrations-info-container" class="w-full  bg-allports-100 shadow-md p-4 rounded-lg flex flex-col justify-center items-center">
+                    <div>Pago Movil 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                        </svg>
+                    </div>
+                    <p class="break-all text-sm text-allports-900 lg:text-base"> 
+                        <span class="font-semibold text-allports-950">Email:</span>  ${unverifiedRegistration[i].email} <br>
+                        <span class="font-semibold text-allports-950">Curso:</span>  ${unverifiedRegistration[i].course} (${'pendiente'})<br>
+                        <span class="font-semibold text-allports-950">Monto:</span>   ${unverifiedRegistration[i].total} ${unverifiedRegistration[i].currency}<br>
+                        <span class="font-semibold text-allports-950">Referencia:</span> #${unverifiedRegistration[i].order_id}
+                    </p>
+                    <div id="btn-container" class="flex gap-4">
+                        <button id="registrations-btn" class="bg-allports-700 hover:bg-allports-800 text-sm p-4 text-allports-50 rounded-lg mt-4 font-semibold">Confirmar</button>
+                        <button id="registrations-btn" class="bg-red-400 hover:bg-red-500 text-sm p-4 text-allports-50 rounded-lg mt-4 font-semibold">Rechazar</button>
+                    </div>
+                </div>
+                `;
+            }
+        }
+        // de no haber pagos relleno el contenedor con un mensaje de ausencia de pagos
+        if (unverifiedRegistration.length === 0) {
+            registrationContainer.innerHTML = `
+            <div id="no-registrations" class="flex justify-center items-center gap-4 bg-allports-50 p-6 rounded-3xl font-semibold text-allports-900">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+                </svg>
+                <p class="text-center">No hay registros pendientes</p>
+            </div>
+            `;
+        }
+
         // finalmente muestro la pantalla principal y escondo la pantalla de carga
         mainContainer.classList.remove('hide');
         loaderScreen.classList.add('hide');
@@ -33,7 +95,7 @@ const loaderScreen = document.querySelector('#loader-screen');
         console.log(error);
 
         // mensaje de error para el usuario en caso de que algo falle
-        textLoader.outerHTML = '<h2 id="text-loader" class="font-extrabold text-2xl text-red-500">Error del servidor :(</h2>';
+        textLoader.outerHTML = `<h2 id="text-loader" class="font-extrabold text-2xl text-red-500">${error.response.data.error}</h2>`;
     }
 })();
 
