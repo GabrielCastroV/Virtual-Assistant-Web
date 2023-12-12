7283c2c2a3c4536fdaa4f60203442820769f467c
Gabriel <gabrielenriquecas@gmail.com>
2023-12-11 21:36:12 -0400

    documentation added

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,42 +8,68 @@ const dashboardRouter = require('express').Router();
 
 dashboardRouter.get('/', async (req, res) => {
     try {
+        // obtengo todos los estudiantes de mi base de datos
         const students = await User.find();
+
+        // obtengo el precio del dolar actualizado
         const info = await getDollarPrices();
+
+        // selecciono el dolar BCV
         const dollarPrice = info[5].dollar;
+
+        // obtengo todos mis recibos de pagos de inscripciones
         const registrations = await Registration.find();
+
+        // obtengo todos los recibos de pagos de pago de módulos
         const pagoMovil = await PagoMovil.find();
+
+        // obtengo todos los cursos disponibles
         const courses = await Course.find();
+
+        // devuelvo una respuesta positiva (OK) al front-end
         return res.status(200).json({ students, dollarPrice, registrations, pagoMovil, courses });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ error: 'Hubo un error en el servidor :(' });
+        // en caso de error, devuelvo una respuesta negativa (Bad request) al front-end
+        return res.status(400).json({ error: 'Hubo un error en el servidor :(' });
     }
 });
 
 dashboardRouter.delete('/:id', async (req, res) => {
     try {
+        // elimino de la base de datos el pago movil
         await PagoMovil.findByIdAndDelete(req.params.id);
+
+        // devuelvo una respuesta positiva al front-end
         return res.status(200).json('Pago de módulo eliminado 🗑️');
     } catch (error) {
+        console.log(error);
+        // en caso de error, devuelvo una respuesta negativa (Bad request) al front-end.
         return res.status(400).json({ error: 'Error al eliminar el pago ✖️' });
     }
 });
 
 dashboardRouter.patch('/:id', async (req, res) => {
     try {
+        // verifico al usuario y obtengo el email para sumarle un modulo y un mes
         const { email } = await PagoMovil.findByIdAndUpdate(req.params.id, { verified: true });
-        const { module, payday } = await User.findOne({ email: email });
-        const upgrated = module + 1;
 
+        // busco al estudiante mediante el email y obtengo solo el modulo y dia de pago del usuario.
+        const { module, payday } = await User.findOne({ email: email });
+
+        // sumo un modulo al estudiante y agrego un mes más de día de pago.
+        const upgrated = module + 1;
         payday.setMonth(payday.getMonth() + 1);
 
-        const savedUser = await User.findOneAndUpdate({ email: email }, { module: upgrated }, { payday: payday });
-        console.log(savedUser);
-        return res.status(200).json('Pago de módulo actualizado!');
+        // finalmente actualizo los datos del estudiante (modulo y dia de pago)
+        await User.findOneAndUpdate({ email: email }, { module: upgrated }, { payday: payday });
+
+        // devuelvo una respuesta positiva (OK) al front-end.
+        return res.status(200).json('Pago de módulo actualizado correctamente :)');
     } catch (error) {
         console.log(error);
-        return res.status(400).json({ error: 'No se ha podido confirmar el pago' });
+        // en caso de error devuelvo una respuesta negativa (Bad request) al front-end
+        return res.status(400).json({ error: 'No se ha podido confirmar el pago :(' });
     }
 });
 
