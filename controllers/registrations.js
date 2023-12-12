cd1fe3288704ee20d205920d98b342e1c79bff4f
Gabriel <gabrielenriquecas@gmail.com>
2023-12-11 21:45:53 -0400

    registrations payment backend added

--- controllers/registrations.js
+++ controllers/registrations.js
@@ -11,5 +11,18 @@ registrationsRouter.delete('/:id', async (req, res) => {
         return res.status(400).json({ error: 'Error al eliminar el pago ✖️' });
     }
 });
+registrationsRouter.patch('/:id', async (req, res) => {
+    try {
+        // verifico el pago de la inscripción
+        await Registration.findByIdAndUpdate(req.params.id, { verified: true });
+
+        // devuelvo una respuesta positiva (OK) al front-end.
+        return res.status(200).json('El pago de inscripción ha sido confirmado correctamente :)');
+    } catch (error) {
+        console.log(error);
+        // en caso de error devuelvo una respuesta negativa (Bad request) al front-end
+        return res.status(400).json({ error: 'No se ha podido confirmar el pago de inscripción :(' });
+    }
+});
 
 module.exports = registrationsRouter;
--- views/dashboard/index.js
+++ views/dashboard/index.js
@@ -309,5 +309,49 @@ registrationContainer.addEventListener('click', async e => {
             deleteLoader.classList.add('hide');
         }
     }
+
+    if (e.target.closest('#confirm-registrations-btn') || e.target.closest('#confirm-registrations-btn')) {
+        const block = e.target.closest('.registrations-info-container');
+        const dataPayment = block.children[1];
+        const loader = block.children[3];
+
+        dataPayment.classList.add('hide');
+        loader.classList.remove('hide');
+
+        try {
+            const { data } = await axios.patch(`/api/registrations/${block.id}`)
+            block.remove();
+            createNotification(false, data);
+            setTimeout(() => {
+                notification.classList.add('hidden');
+            }, 5000);
+            dataPayment.classList.remove('hide');
+            loader.classList.add('hide');
+            pendingRegistration.innerHTML -= 1;
+            // ajusto los contenedores al centro
+            registrationContainer.classList.remove('justify-center');
+            if (pendingRegistration.innerHTML == 0) {
+                registrationContainer.innerHTML = `
+                <div id="no-registrations" class="flex justify-center items-center gap-4 bg-allports-50 p-6 rounded-3xl font-semibold text-allports-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+                    </svg>
+                    <p class="text-center">No hay registros pendientes</p>
+                </div>
+                `;
+                registrationContainer.classList.add('justify-center');
+            } 
+        } catch (error) {
+            console.log(error);
+            createNotification(true, error.response.data.error);
+            setTimeout(() => {
+                notification.classList.add('hidden');
+            }, 5000);
+            dataPayment.classList.remove('hide');
+            loader.classList.add('hide');
+        }
+        
+    }
+
 })
 
